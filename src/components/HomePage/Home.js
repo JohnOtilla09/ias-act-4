14cc41fd503ddf21652a3836518cd2bcc0ea0a0a
JohnOtilla09 <otillajohn09@gmail.com>
2023-07-02 15:03:52 +0800

    edit the homepage

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -2,27 +2,16 @@ import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import styles from './Home.module.css';
 
-const Home = (props) => {
-    const downloadHandler = () => {
-        // using Java Script method to get PDF file
-        fetch('/assets/ENCABO_OTILLA_SOLANOY_ACT-4.pdf').then(response => {
-            response.blob().then(blob => {
-                // Creating new object of PDF file
-                const fileURL = window.URL.createObjectURL(blob);
-                // Setting various property values
-                let alink = document.createElement('a');
-                alink.href = fileURL;
-                alink.download = 'ENCABO_OTILLA_SOLANOY_ACT-4.pdf';
-                alink.click();
-            })
-        })
-    };
+import pdf from '../../assets/ENCABO_OTILLA_SOLANOY_ACT-4.pdf';
 
+// /assets/ENCABO_OTILLA_SOLANOY_ACT-4.pdf
+
+const Home = (props) => {
     return (
         <Card>
             <div className={styles.home}>
             <h1>Welcome to Page</h1>
-            <button className={`${styles['button-64']}`} onClick={downloadHandler}><span class="text">Download</span></button>
+            <a href={pdf} download='ENCABO_OTILLA_SOLANOY_ACT-4' rel="noreffer" ><button className={`${styles['button-64']}`}><span class="text">Download</span></button></a>
             <Button className={styles.Button} onClick={props.onLogout}>Logout</Button>
             </div>
         </Card>
